095ddcc71b9a90385cfe632581d0ae47be1e1ed8
Carson-Reed12 <carsonreed97@gmail.com>
2025-01-14 21:27:34 -0600

    Adding Day3 part 2

--- 2023/Day3/day3.py
+++ 2023/Day3/day3.py
@@ -31,3 +31,38 @@ for i, line in enumerate(lines):
         if checkNumber([i, m.start()]):
             schematic_sum += int(m.group())
 print(f"PART 1 SCHEMATIC SUM: {schematic_sum}")
+
+####### PART 2
+gear_map = {}
+def checkGearNumber(index):
+    directions = [[0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1,1]]
+    try:
+        while board[index[0]][index[1]] in "0123456789":
+            for direction in directions:
+                try:
+                    check_char = board[index[0] + direction[0]][index[1] + direction[1]]
+                    if check_char == "*":
+                        return [index[0] + direction[0], index[1] + direction[1]]
+                except:
+                    pass
+
+            index = [index[0], index[1] + 1]
+        return None
+    except:
+        return None
+    
+for i, line in enumerate(lines):
+    for m in re.finditer("\d+", line.strip()):
+        gear = checkGearNumber([i, m.start()])
+        if gear:
+            try:
+                gear_map[str(gear)].append(int(m.group()))
+            except:
+                gear_map[str(gear)] = [int(m.group())]
+
+schematic_sum = 0
+for gear in gear_map.keys():
+    vals = gear_map[gear]
+    if len(vals) == 2:
+        schematic_sum += vals[0] * vals[1]
+print(f"PART 2 SCHEMATIC SUM: {schematic_sum}")
