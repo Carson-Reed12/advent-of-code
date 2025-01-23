65c8c00fb12178a00542c10bdccc99d7f3ea7e1f
Carson-Reed12 <carsonreed97@gmail.com>
2025-01-23 15:51:07 -0600

    Adding Day8 Part 2

--- 2023/Day8/day8.py
+++ 2023/Day8/day8.py
@@ -1,4 +1,5 @@
 import copy
+import math
 ####### SETUP
 with open("day8_input.txt", "r") as file:
     lines = file.readlines()
@@ -17,3 +18,30 @@ while current_node != "ZZZ":
     current_node = nodes[current_node][steps[step_count % len(steps)]]
     step_count += 1
 print(f"PART 1 STEP COUNT: {step_count}")
+
+####### PART 2
+starting_nodes = []
+for node in nodes:
+    if node[-1] == "A":
+        starting_nodes.append(node)
+node_steps = [0 for _ in range(len(starting_nodes))]
+
+step_count = 0
+while any(node_step == 0 for node_step in node_steps):
+    new_nodes = []
+    for node in starting_nodes:
+        new_nodes.append(nodes[node][steps[step_count % len(steps)]])
+    
+    step_count += 1
+    starting_nodes = copy.deepcopy(new_nodes)
+
+    for i in range(len(starting_nodes)):
+        if starting_nodes[i][-1] == "Z":
+            node_steps[i] = step_count
+    
+lcm = lambda a, b: abs(a * b) // math.gcd(a, b)
+total_steps = node_steps[0]
+for steps in node_steps:
+    if total_steps != steps:
+        total_steps = lcm(total_steps, steps)
+print(f"PART 2 STEP COUNT: {total_steps}")
