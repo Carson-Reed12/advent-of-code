e078d298bf39e7005a41abf9c19f026803d6cabd
Carson-Reed12 <carsonreed97@gmail.com>
2025-02-11 08:38:55 -0600

    Adding day17 part 1

---
+++ 2023/Day17/day17.py
@@ -0,0 +1,50 @@
+import networkx as nx
+####### SETUP
+with open("day17_input.txt", "r") as file:
+    lines = file.readlines()
+
+board = []
+for line in lines:
+    blocks = [int(block) for block in list(line.strip())]
+    board.append(blocks)
+
+SIZE = len(board)
+
+####### PART 1
+G = nx.DiGraph()
+nodes = {}
+
+start_node = (str([0,0]), "E", 2)
+for i, line in enumerate(board):
+    for j, char in enumerate(line):
+        for dir in "NESW":
+            G.add_node((str([i,j]), dir, 2))
+            G.add_node((str([i,j]), dir, 1))
+            G.add_node((str([i,j]), dir, 0))
+
+for dir in "NESW":
+    for level in range(3):
+        G.add_edge((str([SIZE-1,SIZE-1]), dir, level), "end", weight=0)
+
+directions = {"N": [-1,0], "E": [0,1], "S": [1,0], "W": [0,-1]}
+toggle_dir = lambda a : ("S" if a == "N" else "N") if a in ["N", "S"] else ("E" if a == "W" else "W")
+for node, dir, level in G.nodes:
+    if node not in "end":
+        coord0 = int(node.split(",")[0][1:])
+        coord1 = int(node.split(",")[1][:-1])
+
+        remaining_dirs = list("NESW")
+        remaining_dirs.remove(toggle_dir(dir))
+
+        for remaining_dir in remaining_dirs:
+            if (0 <= coord0 + directions[remaining_dir][0] < SIZE) and (0 <= coord1 + directions[remaining_dir][1] < SIZE):
+                next_coord = [coord0 + directions[remaining_dir][0], coord1 + directions[remaining_dir][1]]
+                block = board[coord0 + directions[remaining_dir][0]][coord1 + directions[remaining_dir][1]]
+
+                if remaining_dir != dir:
+                    G.add_edge((node, dir, level), (str(next_coord), remaining_dir, 2), weight=block)
+                elif level != 0:
+                    G.add_edge((node, dir, level), (str(next_coord), dir, level-1), weight=block)
+
+lowest_score = nx.shortest_path_length(G, start_node, "end", weight="weight")
+print(f"PART 1 LOWEST SCORE: {lowest_score}")
