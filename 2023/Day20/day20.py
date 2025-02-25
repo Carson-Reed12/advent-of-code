98a6978cb71e7ac94577456df756865a72e0b242
Carson-Reed12 <carsonreed97@gmail.com>
2025-02-24 20:18:41 -0600

    day20 part 1 cleaning

--- 2023/Day20/day20.py
+++ 2023/Day20/day20.py
@@ -70,7 +70,7 @@ class Broadcaster():
         self.name = name
         self.receivers = receivers
 
-    def sendPulse(self): # need to split into receivePulse and sendPulse because need to queue them instead of auto triggering the next thing. this would be receivePulse and then return True if trigger to add object to queue (kinda)
+    def sendPulse(self):
         modules = []
         for receiver in self.receivers:
             pulse_count["low"] += 1
@@ -95,61 +95,57 @@ class Machine():
             module = self.module_queue.pop(0)
             self.module_queue.extend(module.sendPulse())
 
-# instantiate objects
-flip_flops = []
-conjunctions = []
-pulse_count = {"low": 0, "high": 0}
-for line in lines:
-    if "broadcaster" in line:
-        broadcaster = Broadcaster()
-    elif "%" in line:
-        flip_flops.append(FlipFlop(line.split(" -> ")[0][1:]))
-    elif "&" in line:
-        conjunctions.append(Conjunction(line.split(" -> ")[0][1:]))
+def instantiateMachine():
+    # instantiate objects
+    flip_flops = []
+    conjunctions = []
+    for line in lines:
+        if "broadcaster" in line:
+            broadcaster = Broadcaster()
+        elif "%" in line:
+            flip_flops.append(FlipFlop(line.split(" -> ")[0][1:]))
+        elif "&" in line:
+            conjunctions.append(Conjunction(line.split(" -> ")[0][1:]))
 
-# load connections
-for line in lines:
-    load_receivers = []
-    written_receivers = line.strip().split(" -> ")[1].split(", ")
-    if written_receivers[0] not in [flip_flop.name for flip_flop in flip_flops] and written_receivers[0] not in [conjunction.name for conjunction in conjunctions]:
-        load_receivers.append(FlipFlop(written_receivers[0])) # probably a better way to write this and won't work if multiple 'outputs
+    # load connections
+    for line in lines:
+        load_receivers = []
+        written_receivers = line.strip().split(" -> ")[1].split(", ")
+        if written_receivers[0] not in [flip_flop.name for flip_flop in flip_flops] and written_receivers[0] not in [conjunction.name for conjunction in conjunctions]:
+            load_receivers.append(FlipFlop(written_receivers[0])) # probably a better way to write this and won't work if multiple 'outputs
 
-    for flip_flop in flip_flops:
-        if flip_flop.name in written_receivers:
-            load_receivers.append(flip_flop)
+        for flip_flop in flip_flops:
+            if flip_flop.name in written_receivers:
+                load_receivers.append(flip_flop)
+
+        for conjunction in conjunctions:
+            if conjunction.name in written_receivers:
+                load_receivers.append(conjunction)
+
+        if "broadcaster" in line:
+            broadcaster.updateReceivers(load_receivers)
+        elif "%" in line:
+            name = line.split(" -> ")[0][1:]
+            [flip_flop for flip_flop in flip_flops if flip_flop.name == name][0].updateReceivers(load_receivers)
+        elif "&" in line:
+            name = line.split(" -> ")[0][1:]
+            [conjunction for conjunction in conjunctions if conjunction.name == name][0].updateReceivers(load_receivers)
 
     for conjunction in conjunctions:
-        if conjunction.name in written_receivers:
-            load_receivers.append(conjunction)
+        inputs = []
+        for line in lines:
+            name = line.split(" -> ")[0][1:]
+            receivers = line.strip().split(" -> ")[1].split(", ")
+            if conjunction.name in receivers:
+                inputs.append(name)
+        conjunction.updateInputs(inputs)
 
-    if "broadcaster" in line:
-        broadcaster.updateReceivers(load_receivers)
-    elif "%" in line:
-        name = line.split(" -> ")[0][1:]
-        [flip_flop for flip_flop in flip_flops if flip_flop.name == name][0].updateReceivers(load_receivers)
-    elif "&" in line:
-        name = line.split(" -> ")[0][1:]
-        [conjunction for conjunction in conjunctions if conjunction.name == name][0].updateReceivers(load_receivers)
+    return Machine(broadcaster)
 
-for conjunction in conjunctions:
-    inputs = []
-    for line in lines:
-        name = line.split(" -> ")[0][1:]
-        receivers = line.strip().split(" -> ")[1].split(", ")
-        if conjunction.name in receivers:
-            inputs.append(name)
-    conjunction.updateInputs(inputs)
-
-print([receiver.name for receiver in broadcaster.receivers])
-for flip_flop in flip_flops:
-    print(f"{flip_flop.name} - {[receiver.name for receiver in flip_flop.receivers]}")
-for conjunction in conjunctions:
-    print(f"{conjunction.name} - {[input_name for input_name in conjunction.inputs]} - {[receiver.name for receiver in conjunction.receivers]}")
-
-machine = Machine(broadcaster)
-print("---")
+pulse_count = {"low": 0, "high": 0}
+machine = instantiateMachine()
 for _ in range(1000):
     machine.push_button()
-print(pulse_count)
-print(pulse_count["low"]*pulse_count["high"])
+print(f"PART 1 PULSE COUNT: {pulse_count['low']*pulse_count['high']}")
 
+####### PART 2
